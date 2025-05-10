3c17bffb7f9948de3af3686b04d20fe75f2356b8
maustiel <maurine.meeus@ifosup.wavre.be>
2025-05-10 17:35:30 +0200

    acess contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -55,7 +55,7 @@
                 alt="Icône Téléphone"
                 class="w-10 h-10 mb-4"
               />
-              <h3 class="text-xl font-semibold mb-2">Téléphone</h3>
+              <h2 class="text-xl font-semibold mb-2">Téléphone</h2>
               <a href="tel:+32492395590" class="hover:underline"
                 >0492 39 55 90</a
               >
@@ -69,7 +69,7 @@
                 alt="Icône E-mail"
                 class="w-10 h-10 mb-4"
               />
-              <h3 class="text-xl font-semibold mb-2">E-mail</h3>
+              <h2 class="text-xl font-semibold mb-2">E-mail</h2>
               <a
                 href="mailto:Mimie.Brindille@hotmail.com"
                 class="hover:underline"
@@ -85,7 +85,7 @@
                 alt="Icône Adresse"
                 class="w-10 h-10 mb-4"
               />
-              <h3 class="text-xl font-semibold mb-2">Adresse</h3>
+              <h2 class="text-xl font-semibold mb-2">Adresse</h2>
               <p class="text-center">
                 Pl. de Liberchies 15<br />
                 6238 Pont-à-Celles, Belgique
@@ -100,7 +100,7 @@
                 alt="Icône Horaires"
                 class="w-10 h-10 mb-4"
               />
-              <h3 class="text-xl font-semibold mb-2">Horaires</h3>
+              <h2 class="text-xl font-semibold mb-2">Horaires</h2>
               <ul class="self-stretch list-disc pl-5 space-y-1 text-left">
                 <li>Lun-Ven : 9h00 – 18h00</li>
                 <li>Mercredi : Fermé</li>
@@ -117,6 +117,7 @@
               class="w-full h-full border-0"
               allowfullscreen
               loading="lazy"
+              title="Carte interactive google maps indiquant l'emplacement de la boutique Mimie Brindille à Pont-à-Celles"
             ></iframe>
           </div>
         </div>
@@ -254,50 +255,63 @@
     </div>
   </div>
   <!-- Suivez-Moi -->
-<div class="py-5 px-4">
-  <h2 class="text-3xl font-bold text-[#CB8587] mb-4 text-center">
-    Suivez-moi !
-  </h2>
-  <p class="louis text-lg mb-6 text-center">
-    N’hésitez pas à me suivre sur les réseaux sociaux pour découvrir les dernières créations et les actus de la boutique.
-  </p>
-  <div class="flex justify-center space-x-6">
-    <!-- Facebook -->
-    <a
-      href="https://www.facebook.com/people/Mimie-Brindille/100091998159115/"
-      target="_blank"
-      aria-label="Facebook"
-      class="text-[#CB8587] hover:text-[#e09698] transition"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-8 h-8 fill-current">
-        <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/>
-      </svg>
-    </a>
-    <!-- Instagram -->
-    <a
-      href="https://www.instagram.com/brindillemimie/"
-      target="_blank"
-      aria-label="Instagram"
-      class="text-[#CB8587] hover:text-[#e09698] transition"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-8 h-8 fill-current">
-        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-      </svg>
-    </a>
+  <div class="py-5 px-4">
+    <h2 class="text-3xl font-bold text-[#CB8587] mb-4 text-center">
+      Suivez-moi !
+    </h2>
+    <p class="louis text-lg mb-6 text-center">
+      N’hésitez pas à me suivre sur les réseaux sociaux pour découvrir les
+      dernières créations et les actus de la boutique.
+    </p>
+    <div class="flex justify-center space-x-6">
+      <!-- Facebook -->
+      <a
+        href="https://www.facebook.com/people/Mimie-Brindille/100091998159115/"
+        target="_blank"
+        aria-label="Facebook"
+        class="text-[#CB8587] hover:text-[#e09698] transition"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+          class="w-8 h-8 fill-current"
+        >
+          <path
+            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+          />
+        </svg>
+      </a>
+      <!-- Instagram -->
+      <a
+        href="https://www.instagram.com/brindillemimie/"
+        target="_blank"
+        aria-label="Instagram"
+        class="text-[#CB8587] hover:text-[#e09698] transition"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="w-8 h-8 fill-current"
+        >
+          <path
+            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+          />
+        </svg>
+      </a>
+    </div>
   </div>
-</div>
-
 </template>
 
 <script setup>
-import { useHead } from '#imports'
+import { useHead } from "#imports";
 
 useHead({
   title: "Contact - Mimie Brindille, Fleuriste à Pont-à-Celles",
   meta: [
     {
       name: "description",
-      content: "Besoin d’un bouquet personnalisé ou d’un renseignement ? Contactez-moi facilement par téléphone, mail ou via le formulaire. Je vous réponds avec plaisir depuis ma boutique à Pont-à-Celles.",
+      content:
+        "Besoin d’un bouquet personnalisé ou d’un renseignement ? Contactez-moi facilement par téléphone, mail ou via le formulaire. Je vous réponds avec plaisir depuis ma boutique à Pont-à-Celles.",
     },
     {
       property: "og:title",
@@ -305,7 +319,8 @@ useHead({
     },
     {
       property: "og:description",
-      content: "Prenez contact avec Mimie Brindille pour toute demande florale, commande personnalisée ou projet événementiel.",
+      content:
+        "Prenez contact avec Mimie Brindille pour toute demande florale, commande personnalisée ou projet événementiel.",
     },
     {
       property: "og:image",
@@ -318,16 +333,15 @@ useHead({
     {
       name: "robots",
       content: "index, follow",
-    }
+    },
   ],
   link: [
     {
       rel: "canonical",
       href: "https://mimiebrindille.be/contact",
-    }
-  ]
-})
-
+    },
+  ],
+});
 </script>
 
 <style scoped>
