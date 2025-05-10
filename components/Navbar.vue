169a197e66d9b3c7cd84f08aa898d100a828d379
maustiel <maurine.meeus@ifosup.wavre.be>
2025-05-10 14:20:57 +0200

    16e mise en ligne oscour

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -80,7 +80,7 @@
     <div class="relative overflow-visible">
       <!-- autorise l’overflow vertical -->
       <div
-        class="absolute top-[-40px] left-0 w-full h-[100px] bg-cover bg-center bg-no-repeat z-10"
+        class="absolute top-[-40px] left-0 w-full h-[95px] bg-cover bg-center bg-no-repeat z-10"
         style="background-image: url('/images/fleurs-navbar.webp')"
       ></div>
     </div>
